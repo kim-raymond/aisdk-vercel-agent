383eb45632617c0c6d1fa01c747fcf0afe895ee0
mon <magallanesraymond27@gmail.com>
2026-06-29 13:54:16 +0800

    cookies error fix

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,6 +1,7 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+export const dynamic = 'force-dynamic';
 export async function createClient() {
   const cookieStore = await cookies()
 
